4b92db02e6f4bfcb24bb313a89b83d13398e40ca
Francisco Vargas Ruiz <4354486+fvarrui@users.noreply.github.com>
2025-04-07 07:02:27 +0100

    Update __main__.py

--- src/dbanalyzer/__main__.py
+++ src/dbanalyzer/__main__.py
@@ -1,4 +1,5 @@
 import sys
+import json
 from openai import OpenAI
 
 from dbschema.schema import Schema
@@ -16,39 +17,47 @@ def main():
         # Inicializar el cliente de OpenAI
         client = OpenAI(api_key=apikey)
 
-        # Subir el archivo JSON al servidor
-        file = client.files.create(
-            file=open("schemas/pec.json", "rb"),
-            purpose="user_data"
-        )
+        #Listar modelos soportados
+        #models = client.models.list()
+        #print("Modelos soportados:")
+        #for model in models.data:
+        #    print(f"- {model.id}")
+
+        schema = Schema.from_json("schemas/pec.json")
+
+        # Generar un mensaje simplificado por cada tabla
+        messages = [
+            {
+                "type": "input_text",
+                "text": json.dumps({
+                    "table_name": table.name,
+                    "columns": [{"name": col.name, "type": col.type} for col in table.columns]
+                }),
+            } for table in schema.tables
+        ]
+        messages.append({
+            "type": "input_text",
+            "text": """
+                Usa inferencia para determinar que me des los comentarios sobre todas 
+                las tablas y columnas que te he pasado como un esquema,
+                indican la información que contienen y su relación con otras tablas.
+                Es necesario que la salida sea un JSON válido. La base de datos es el 
+                Plan de Estudios de Canarias.
+            """,
+        })
 
         # Crear el historial de mensajes
         conversation = [
             {
                 "role": "user",
-                "content": [
-                    {
-                        "type": "input_file",
-                        "file_id": file.id,
-                    },
-                    {
-                        "type": "input_text",
-                        "text": """
-                            Quiero que me des el mismo esquema pero con los comentarios de tablas y columnas.
-                            Es necesario que la salida sea un JSON válido. Usa la inferencia, en función
-                            de las relaciones y los nombres y tipos de datos. La base de datos es el 
-                            Plan de Estudios de Canarias. Si necesitas datos de las tablas, dímelo.
-                        """,
-                    },
-                ]
+                "content": messages
             }
         ]
 
         # Enviar la solicitud inicial
         response = client.responses.create(
-            model="gpt-4o",
+            model= "gpt-4o",
             instructions="Eres un experto en bases de datos y JSON.",
-            response_format="json",
             input=conversation
         )
 
